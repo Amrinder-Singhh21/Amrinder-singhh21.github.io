adedb1afbbfcb43c36bb6e4bba3b0303778b9c06
Recraft-9 <recraft-9@Recraft-9s-MacBook-Air.local>
2021-10-03 13:51:29 +0530

    Updates

---
+++ precache-manifest.18f5721a585c30e976a557daaa258be1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e899e502239a02a6c75ccb2ce6ad275",
+    "url": "/17oct2021/index.html"
+  },
+  {
+    "revision": "2712d75dd19e120df858",
+    "url": "/17oct2021/static/js/2.63351b38.chunk.js"
+  },
+  {
+    "revision": "cdec75cf36ab526a1971",
+    "url": "/17oct2021/static/js/main.b1cf1509.chunk.js"
+  },
+  {
+    "revision": "f20907c0ced555dd87e7",
+    "url": "/17oct2021/static/js/runtime-main.83407c44.js"
+  }
+]);
